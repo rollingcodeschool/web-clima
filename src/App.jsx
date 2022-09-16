d7c4be5e02375dac71b6c872084218b705b93af9
Emilse Arias <emilsearias@gmail.com>
2022-09-16 12:50:40 -0300

    add component datacard and form

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherForm from "./components/WeatherForm";
 import DataCard from "./components/DataCard";
+import { useState } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Error from "./components/Error";
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [error, setError] = useState(false)
   return (
     <>
       <main className=" container my-5">
-        <WeatherForm></WeatherForm>
-        <DataCard></DataCard>
+        <WeatherForm setWeatherData={setWeatherData} setError={setError}></WeatherForm>
+        { !error ? (
+          <DataCard weatherData={weatherData}></DataCard>
+        ) : (
+          <Error />
+        )}
       </main>
       <footer className="bg-dark text-light text-center py-4">
         <p>&copy; Todos los derechos reservados</p>
--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,18 +1,38 @@
 import React from "react";
-import { Card, Badge } from "react-bootstrap";
+import { Card, Badge, Row, Col } from "react-bootstrap";
 
-const DataCard = () => {
+const DataCard = ({ weatherData }) => {
   return (
     <Card>
-      <Card.Body>
+      <Card.Body className="text-center">
         <Card.Title>
-          City <Badge bg="warning" text="dark">Country</Badge>
+          {weatherData.name}{" "}
+          <Badge bg="warning" text="dark">
+            {weatherData.sys?.country}
+          </Badge>
         </Card.Title>
-        <Card.Text>
-         <h2>9 C</h2>
-         <img src="" alt="" />
-         Lorem ipsum dolor sit amet.
-        </Card.Text>
+        {weatherData?.weather ? (
+          <img
+            src={` http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+            alt={weatherData?.weather[0]?.description}
+          />
+        ) : null}
+        <h2>{weatherData.main?.temp} °C</h2>
+        <span className="lead">
+          {weatherData?.weather ? weatherData.weather[0].description : null}
+        </span>
+        <hr />
+
+        <Row>
+          <Col>
+            <p className="fw-bold">Minima</p>
+            <p>{weatherData.main?.temp_min} °C</p>
+          </Col>
+          <Col>
+            <p className="fw-bold">Maxima</p>
+            <p>{weatherData.main?.temp_max} °C</p>
+          </Col>
+        </Row>
       </Card.Body>
     </Card>
   );
